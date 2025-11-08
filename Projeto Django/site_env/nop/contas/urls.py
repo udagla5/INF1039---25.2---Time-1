d0e96e8cd204b1e8ac7b0971bd26a63686693662
Pedro Henrique Vasconcelos Martins <pedrohenrique873@hotmail.com>
2025-11-07 21:51:22 -0300

    Atualização pasta static

--- Projeto Django/site_env/nop/contas/urls.py
+++ Projeto Django/site_env/nop/contas/urls.py
@@ -3,4 +3,12 @@ from . import views
 urlpatterns = [
     path('cadastro1/', views.cadastro1, name='cadastro1'),
     path('cadastro2/', views.cadastro2, name='cadastro2'),
+    path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('perfil_aluno/', views.perfil_aluno, name='perfil_aluno'),
+    path('perfil_aluno_parte2/', views.perfil_aluno_parte2, name='perfil_aluno_parte2'),
+    path('feed/', views.feed, name='feed'),
+    path('criar_oportunidade1/', views.criar_oportunidade1, name='criar_oportunidade1'),
+    path('criar_oportunidade2/', views.criar_oportunidade2, name='criar_oportunidade2'),
+    path('criar_oportunidade3/', views.criar_oportunidade3, name='criar_oportunidade3'),
 ]
--- Projeto Django/site_env/nop/contas/views.py
+++ Projeto Django/site_env/nop/contas/views.py
@@ -6,3 +6,27 @@ def cadastro1(request):
 
 def cadastro2(request):
     return render(request, 'cadastro2.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def perfil_aluno(request):
+    return render(request, 'perfil_aluno.html')
+
+def perfil_aluno_parte2(request):
+    return render(request, 'perfil_aluno_parte2.html')
+
+def feed(request):
+    return render(request, 'feed.html')
+
+def criar_oportunidade1(request):
+    return render(request, 'criar_oportunidade1.html')
+
+def criar_oportunidade2(request):
+    return render(request, 'criar_oportunidade2.html')
+
+def criar_oportunidade3(request):
+    return render(request, 'criar_oportunidade3.html')
